f5bd493928124a0d3e91f77d2e5ba6cf834a49db
Vikrant Gupta <vikrant.gupta@messagemedia.com.au>
2021-07-06 21:45:15 +1000

    Initial commit - skeleton spring boot 2 batch ;

---
+++ src/test/java/com/viks/intuit/craft_tradie/CraftTradieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viks.intuit.craft_tradie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftTradieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
